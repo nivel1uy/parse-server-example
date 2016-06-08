330ebd430e01d3f964f0c78ad38040ef3429bc27
nivel1uy <aplicacionnivel1@gmail.com>
2016-06-08 16:46:35 -0300

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,9 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.beforeSave("_User", function(request, response) {
+    request.object.set("email", "a@siodgs.com");
+
+    response.success();
+});
